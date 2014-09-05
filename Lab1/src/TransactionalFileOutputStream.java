c2f993658cde87c472bc26a483c5ed8c73179dc7
zhiyiting <zhiyiting@gmail.com>
2014-09-04 22:05:49 -0400

    add functioins for transactionalfileoutputstream

--- Lab1/src/TransactionalFileOutputStream.java
+++ Lab1/src/TransactionalFileOutputStream.java
@@ -1,3 +1,4 @@
+import java.io.RandomAccessFile;
 import java.io.IOException;
 
 public class TransactionalFileOutputStream extends java.io.OutputStream
@@ -5,15 +6,42 @@ public class TransactionalFileOutputStream extends java.io.OutputStream
 
 	private static final long serialVersionUID = -2759612490300531241L;
 	private String filename;
+	private long position;
 
 	public TransactionalFileOutputStream(String arg, boolean val) {
 		this.filename = arg;
+		this.position = 0;
 	}
 
 	@Override
 	public void write(int b) throws IOException {
-		// TODO Auto-generated method stub
+		RandomAccessFile out = new RandomAccessFile(filename, "rws");
+		out.seek(position);
+		out.write(b);
+		out.close();
+		position++;
+	}
 
+	@Override
+	public void write(byte b[]) throws IOException {
+		write(b, 0, b.length);
+	}
+
+	@Override
+	public void write(byte b[], int off, int len) throws IOException {
+		if (b == null) {
+			throw new NullPointerException();
+		} else if ((off < 0) || (off > b.length) || (len < 0)
+				|| ((off + len) > b.length) || ((off + len) < 0)) {
+			throw new IndexOutOfBoundsException();
+		} else if (len == 0) {
+			return;
+		}
+		RandomAccessFile out = new RandomAccessFile(filename, "rws");
+		out.seek(position);
+		out.write(b, off, len);
+		out.close();
+		position += len;
 	}
 
 	@Override
